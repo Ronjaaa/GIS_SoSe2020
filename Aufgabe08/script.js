a1157b89601479cd82920942c2f9ae3e4d952aab
Ronja Kerstin Slabon <ronja.slabon@gmail.com>
2020-06-21 16:26:31 +0200

    bug fix

--- Aufgabe08/script.js
+++ Aufgabe08/script.js
@@ -5,23 +5,19 @@ var Aufgabe08;
     let newButton = document.getElementById("button");
     newButton.addEventListener("click", handleButton);
     //Die Formulardaten werden an den URL gehängt
-    async function addToURL() {
+    async function addToURL(_url) {
         formData = new FormData(document.forms[0]);
-        let url = "https://gissoserosl.herokuapp.com/";
-        //let url: string = "http://localhost:8100";
         let query = new URLSearchParams(formData);
-        url = url + "?" + query.toString();
-        return url;
-    }
-    async function handleButton() {
-        getResponse(await addToURL());
-    }
-    //Die Antwort wird vom Server geholt
-    async function getResponse(_url) {
+        _url = _url + "?" + query.toString();
+        //Die Antwort wird vom Server geholt
         let response1 = await fetch(_url, { method: "get" });
         let response2 = await response1.text();
         console.log(response2);
         console.log("test123");
+    }
+    function handleButton(_event) {
+        addToURL("https://gissoserosl.herokuapp.com/");
+        //addToURL("http://localhost:8100");
     }
 })(Aufgabe08 || (Aufgabe08 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe08/script.ts
+++ Aufgabe08/script.ts
@@ -5,24 +5,25 @@ namespace Aufgabe08 {
     newButton.addEventListener("click", handleButton);
     
     //Die Formulardaten werden an den URL gehängt
-    async function addToURL(): Promise<string> {
+    async function addToURL(_url: RequestInfo): Promise<void> {
         formData = new FormData(document.forms[0]);
-        let url: string = "https://gissoserosl.herokuapp.com/";
-        //let url: string = "http://localhost:8100";
+        
         let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url = url + "?" + query.toString();
-        return url;
-    }
-
-    async function handleButton(): Promise<void> {
-        getResponse(await addToURL());
-    }
-
+        _url = _url + "?" + query.toString();
     //Die Antwort wird vom Server geholt
-    async function getResponse(_url: RequestInfo): Promise<void> {
         let response1: Response = await fetch(_url, { method: "get" });
         let response2: string = await response1.text();
         console.log(response2);
         console.log("test123");
+
     }
-}
+
+    function handleButton(_event: Event): void {
+        addToURL("https://gissoserosl.herokuapp.com/");
+        //addToURL("http://localhost:8100");
+    }
+
+    
+    
+       
+    }
