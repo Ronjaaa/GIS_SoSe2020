ed73d6a25187cbe0cbfca40f792aa0e3f7e792f3
Unknown <ronja.slabon@gmail.com>
2020-07-26 14:39:44 +0200

    ?

--- Endabgabe/verkäufer.js
+++ Endabgabe/verkäufer.js
@@ -3,12 +3,16 @@ var Endabgabe;
 (function (Endabgabe) {
     let newButton = document.getElementById("bestellungenAnzeigen");
     newButton.addEventListener("click", handleNewButton);
+    let text = document.getElementById("bestellungen");
     let formData;
     async function handleNewButton() {
         // tslint:disable-next-line: no-any
         let query = new URLSearchParams(formData);
         let url = "https://gissoserosl.herokuapp.com";
         url = url + "/holen" + "?" + query.toString();
+        let response = await fetch(url);
+        let responsestring = await response.text();
+        text.innerHTML = responsestring;
     }
 })(Endabgabe || (Endabgabe = {}));
 //# sourceMappingURL=verkäufer.js.map
--- Endabgabe/verkäufer.ts
+++ Endabgabe/verkäufer.ts
@@ -1,12 +1,17 @@
 namespace Endabgabe {
     let newButton: HTMLButtonElement = (<HTMLButtonElement>document.getElementById("bestellungenAnzeigen"));
     newButton.addEventListener("click", handleNewButton);
+    let text: HTMLElement = <HTMLElement>document.getElementById("bestellungen");
     let formData: FormData;
+    
     async function handleNewButton(): Promise<void> {
         // tslint:disable-next-line: no-any
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         let url: string = "https://gissoserosl.herokuapp.com";
         url = url + "/holen" + "?" + query.toString();
+        let response: Response = await fetch(url);
+        let responsestring: string = await response.text();
+        text.innerHTML = responsestring;
 
     }
 
